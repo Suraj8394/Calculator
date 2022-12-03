6ca09509e8d5d5a40539b4dd3aaaaf839f4f86d2
Suraj8394 <118334169+Suraj8394@users.noreply.github.com>
2022-12-03 19:48:53 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+x = int(input("my first number is="))
+y = int(input("my second number is="))
+c= x+y
+print("sum",c)
+v=x-y
+print("substract",v)
+b=x*y
+print("product",b)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)#same as 2*2*2*2*2
+print(x//y)
